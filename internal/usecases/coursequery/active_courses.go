694fddf0397627d89ed44a17b4887524eadca937
thuanle <thuanle@hcmut.edu.vn>
2025-06-02 19:53:43 +0700

    fix active course usecase

--- internal/usecases/coursequery/active_courses.go
+++ internal/usecases/coursequery/active_courses.go
@@ -8,19 +8,17 @@ import (
 type ActiveCourseService struct {
 	CourseRepo course.Repository
 	Rule       *course.Rules
-	Now        func() time.Time
 }
 
 func NewActiveCourseService(r course.Repository, rule *course.Rules) *ActiveCourseService {
 	return &ActiveCourseService{
 		CourseRepo: r,
 		Rule:       rule,
-		Now:        time.Now,
 	}
 }
 
 func (s *ActiveCourseService) ListActiveCourses() ([]course.Model, error) {
-	threshold := s.Now().Add(-s.Rule.CourseActiveAge)
+	threshold := time.Now().Add(-s.Rule.CourseActiveAge)
 
 	actives, err := s.CourseRepo.FindCoursesUpdatedAfter(threshold)
 	if err != nil {
